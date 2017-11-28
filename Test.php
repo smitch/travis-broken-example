6acd744eb8aecda2ffdeaecd38ac8d399deba004
smitch <kouichi.sumizaki@gmail.com>
2017-11-28 10:20:39 +0900

    1+1 must be 2

--- Test.php
+++ Test.php
@@ -3,7 +3,8 @@
 class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
-		$this->assertEquals(1+1,3);
+		$this->assertEquals(1+1,2);
+//		$this->assertEquals(1+1,3);
   	}
 }
 
